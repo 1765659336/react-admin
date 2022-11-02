2cc5c1fe5a2fffa8d4cfdf5d3a64cd320e3a466a
JIE <1765659336@qq.com>
2022-11-02 22:35:27 +0800

    [ADD]添加路由懒加载

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,15 @@
+import React, { lazy } from "react";
 import Home from "../views/Home";
-import About from "../views/About";
+const About = lazy(() => import("../views/About"));
 
 // Navigate重定向组件
 import { Navigate } from "react-router-dom";
+
+const withLoadingComponent = (comp: JSX.Element) => {
+  return (
+    <React.Suspense fallback={<div>loading...</div>}>{comp}</React.Suspense>
+  );
+};
 
 const routes = [
   {
@@ -15,7 +22,7 @@ const routes = [
   },
   {
     path: "/about",
-    element: <About />,
+    element: withLoadingComponent(<About />),
   },
 ];
 
