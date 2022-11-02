5041aa7cc1d9c76306084a6daa3e791900389126
Liu Jie <1765659336@qq.com>
2022-11-02 16:58:33 +0800

    [ADD]新增css预处理器scss以及禁用文字复制和图片拖动

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// 初始化样式
 import "reset-css";
+// Ui框架的样式
+
+// 系统样式
+import "./assets/styles/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
