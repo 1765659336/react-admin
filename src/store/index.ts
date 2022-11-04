c249270507acb18de3f5ab69adbab67ca1786ec3
Liu Jie <1765659336@qq.com>
2022-11-04 09:33:21 +0800

    [ADD]使google正常使用redux开发者工具

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
 import { legacy_createStore } from "redux";
 import reducer from "./reducer";
 
+// 为了让浏览器正常使用redux-devetools
 const store = legacy_createStore(
   reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
