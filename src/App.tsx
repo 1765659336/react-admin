0fabd497b0073d493ef55c84d6b842a0e1ecd535
JIE <1765659336@qq.com>
2022-11-05 15:10:03 +0800

    [ADD]前置路由守卫

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
-import React from "react";
-import { useRoutes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useRoutes, useLocation, useNavigate } from "react-router-dom";
 import router from "src/router";
-const App: React.FC = () => {
+import { message } from "antd";
+
+const Page: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    message.warning("你已登录");
+    navigate("/home/users");
+  });
+
+  return <></>;
+};
+
+const Login: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    message.warning("请先登录再访问");
+    navigate("/login");
+  });
+
+  return <></>;
+};
+
+// 前置路由守卫
+const BeforeRouterEnter: React.FC = () => {
   const outlet = useRoutes(router);
+  const location = useLocation();
+  const token = localStorage.getItem("token");
+  if (location.pathname === "/login" && token) {
+    return <Page />;
+  }
+  if (location.pathname !== "/login" && !token) {
+    return <Login />;
+  }
   return <>{outlet}</>;
+};
+
+const App: React.FC = () => {
+  return <BeforeRouterEnter />;
 };
 
 export default App;
