b9e8c866e072250d87389f10d2c2084be59b2162
Liu Jie <1765659336@qq.com>
2022-11-17 11:36:42 +0800

    [ADD]添加less依赖

--- src/components/Cop1/index.tsx
+++ src/components/Cop1/index.tsx
@@ -1,6 +1,13 @@
 import style from "./index.module.scss";
+import style2 from "./index.module.less";
+
 function Cop1() {
-  return <div className={style.box}>cop1</div>;
+  return (
+    <>
+      <div className={style.box}>cop1</div>
+      <div className={style2.box2}>cop1</div>
+    </>
+  );
 }
 
 export default Cop1;
