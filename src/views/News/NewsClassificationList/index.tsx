08cd5af51720d0531f19809f1c35dea05d115e89
JIE <1765659336@qq.com>
2022-11-24 14:38:13 +0800

    [UPDATE]完善新闻审核部分

--- src/views/News/NewsClassificationList/index.tsx
+++ src/views/News/NewsClassificationList/index.tsx
@@ -1,110 +1,126 @@
-import { message, Space, Table, Tag } from 'antd';
-import { ColumnsType } from 'antd/es/table';
-import React from 'react';
-import { useNavigate, useLocation } from 'react-router';
+import { message, Space, Table, Tag } from "antd";
+import { ColumnsType } from "antd/es/table";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router";
+import TableSkeleton from "src/components/TableSkeleton";
+import { getNews, putNewsStatus } from "src/request/News";
 
 interface DataType {
-    id: string,
-    key: string;
-    newsTitle: string;
-    tags: string[];
-    newsStatus: string
+  id: string;
+  key: string;
+  newsTitle: string;
+  newsClassificationText: string;
+  newsStatus: number;
 }
 
-const NewsClassificationList: React.FC = () => {
-
-    const navigate = useNavigate();
-
-    const location = useLocation();
-
-    // 发布审核
-    const release = (id: string) => {
-        console.log(id);
-        // 调用发布审核的接口，并重新刷新页面
-        message.success("发布成功" + id)
-    }
-
-    // 编辑
-    const shelves = (id: string) => {
-        console.log(id);
-        message.success("下架成功" + id)
-    }
-
-    // 查看
-    const view = (id: string) => {
-        console.log(id);
-        navigate("/news/manage/newsItem/details", { replace: false, state: { id, type: "view", previousPath: location.pathname, previousTitle: "审核发布新闻详情查看" } });
-    }
-
-    const dataSource: DataType[] = [
-        {
-            id: '001',
-            key: '1',
-            newsTitle: 'John Brown',
-            tags: ['热点'],
-            newsStatus: '未发布'
-        },
-        {
-            id: '002',
-            key: '2',
-            newsTitle: 'Jim Green',
-            tags: ['民生'],
-            newsStatus: '未发布'
-        },
-        {
-            id: '003',
-            key: '3',
-            newsTitle: 'Joe Black',
-            tags: ['军事'],
-            newsStatus: '已发布'
-        },
-    ];
-
-    const columns: ColumnsType<DataType> = [
-        {
-            title: '新闻标题',
-            dataIndex: 'newsTitle',
-            key: 'newsTitle',
-        },
-        {
-            title: '新闻分类',
-            key: 'tags',
-            dataIndex: 'address',
-            render: (_, { tags }) => (
-                <>
-                    {tags.map(tag => {
-                        return (
-                            <Tag color={'volcano'} key={tag}>
-                                {tag.toUpperCase()}
-                            </Tag>
-                        );
-                    })}
-                </>
-            ),
-        },
-        {
-            title: '新闻状态',
-            dataIndex: 'newsStatus',
-            key: 'newsStatus',
-        },
-        {
-            title: '操作',
-            key: 'action',
-            render: (_, record) => {
-                return (
-                    <Space size="middle">
-                        <a onClick={() => release(record.id)}>发布</a>
-                        <a onClick={() => shelves(record.id)}>下架</a>
-                        <a onClick={() => view(record.id)}>查看</a>
-                    </Space >
-                )
-            },
-        },
-    ];
-
-    return (
-        <Table columns={columns} dataSource={dataSource} />
-    )
+interface Props {
+  setIsSkeleton: Function;
 }
+
+const NewsClassificationListPure: React.FC<Props> = ({ setIsSkeleton }) => {
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const [dataSource, setDataSource] = useState([]);
+
+  // 下架
+  const changeNewsStatus = (id: string, newsStatus: number) => {
+    putNewsStatus({ id, newsStatus }).then((res: any) => {
+      const { status } = res.data;
+      if (status) {
+        message.success("操作成功");
+        init();
+      }
+    });
+  };
+
+  // 查看
+  const view = (id: string) => {
+    console.log(id);
+    navigate("/news/manage/newsItem/details", {
+      replace: false,
+      state: {
+        id,
+        type: "view",
+        previousPath: location.pathname,
+        previousTitle: "审核发布新闻详情查看",
+      },
+    });
+  };
+
+  const init = () => {
+    setIsSkeleton(false);
+    getNews().then((res: any) => {
+      const { status, content } = res.data;
+      if (status) {
+        const transferData = content.map(
+          (item: { id: string; key: string }) => {
+            item.key = item.id;
+            return item;
+          }
+        );
+        setDataSource(transferData);
+        setIsSkeleton(true);
+      }
+    });
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: "新闻标题",
+      dataIndex: "newsTitle",
+      key: "newsTitle",
+    },
+    {
+      title: "新闻分类",
+      key: "newsClassificationText",
+      dataIndex: "newsClassificationText",
+      render: (_, { newsClassificationText }) => (
+        <Tag color={"volcano"} key={newsClassificationText}>
+          {newsClassificationText.toUpperCase()}
+        </Tag>
+      ),
+    },
+    {
+      title: "新闻状态",
+      dataIndex: "newsStatusText",
+      key: "newsStatusText",
+    },
+    {
+      title: "操作",
+      key: "action",
+      render: (_, { newsStatus, id }) => {
+        return (
+          <Space size="middle">
+            {newsStatus === 2 ? (
+              <a onClick={() => changeNewsStatus(id, 4)}>审核通过</a>
+            ) : null}
+            {newsStatus === 2 ? (
+              <a onClick={() => changeNewsStatus(id, 3)}>驳回</a>
+            ) : null}
+            {newsStatus === 4 ? (
+              <a onClick={() => changeNewsStatus(id, 5)}>发布</a>
+            ) : null}
+            {newsStatus === 5 ? (
+              <a onClick={() => changeNewsStatus(id, 4)}>下架</a>
+            ) : null}
+            <a onClick={() => view(id)}>查看</a>
+          </Space>
+        );
+      },
+    },
+  ];
+
+  return <Table columns={columns} dataSource={dataSource} />;
+};
+
+const NewsClassificationList = () => {
+  return <TableSkeleton Comp={NewsClassificationListPure} />;
+};
 
 export default NewsClassificationList;
